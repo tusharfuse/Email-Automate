0bca7bc26d324c6820ccde1e070a4033587531c4
tusharfuse <tusharfuse1802.com>
2025-07-13 10:11:38 +0530

    Add explicit favicon.ico URL pattern to fix 400 error on favicon request

--- missingclaims/urls.py
+++ missingclaims/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inquiry.urls')),
-]
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
